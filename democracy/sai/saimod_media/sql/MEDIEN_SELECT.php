79323c5a4e14c6954add760c59ec05fcb3b17c48
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-06-20 21:00:40 +0200

    changed order for media

--- democracy/sai/saimod_media/sql/MEDIEN_SELECT.php
+++ democracy/sai/saimod_media/sql/MEDIEN_SELECT.php
@@ -4,6 +4,6 @@ namespace SQL;
 class MEDIEN_SELECT extends \SYSTEM\DB\QP {
     public static function get_class(){return \get_class();}
     public static function mysql(){return
-'SELECT * FROM media WHERE type = ? ORDER by `order`;';
+'SELECT * FROM media WHERE type = ? ORDER by `order` DESC;';
     }
 }
